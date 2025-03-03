9408c3cb7ea405da330bfa8fafc3afa0aeea1f31
Yasin Silavi <ys.silavi@gmail.com>
2025-03-03 06:15:00 +0330

    feat: handle back

--- App.tsx
+++ App.tsx
@@ -1,8 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { WebView } from "react-native-webview";
-import { SafeAreaView, View, StyleSheet } from "react-native";
+import React, { useRef, useState, useEffect } from "react";
+import { SafeAreaView, View, StyleSheet, BackHandler } from "react-native";
 
 export default function App() {
+  const webViewRef = useRef(null);
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  useEffect(() => {
+    const backAction = () => {
+      if (!(canGoBack && webViewRef.current)) {
+        return;
+      }
+
+      webViewRef.current.goBack();
+      return true; // Prevent app from closing
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => {
+      backHandler.remove();
+    };
+  }, [canGoBack]);
+
   return (
     <View style={styles.container}>
       <SafeAreaView style={{ flex: 1, backgroundColor: "#2e2e2e" }}>
@@ -11,6 +35,7 @@ export default function App() {
           userAgent="Fenjoon-WebView"
           source={{ uri: "https://fenjoon.vercel.app" }}
           style={{ flex: 1, backgroundColor: "#2e2e2e" }}
+          onNavigationStateChange={({ canGoBack }) => setCanGoBack(canGoBack)}
         />
         <StatusBar />
       </SafeAreaView>
