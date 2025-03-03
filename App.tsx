7786a816682e24868eb63cbacff57cce37e939c3
Yasin Silavi <ys.silavi@gmail.com>
2025-03-03 06:12:33 +0330

    finally a working app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { WebView } from "react-native-webview";
+import { SafeAreaView, View, StyleSheet } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#2e2e2e" }}>
+        <WebView
+          bounces={false}
+          userAgent="Fenjoon-WebView"
+          source={{ uri: "https://fenjoon.vercel.app" }}
+          style={{ flex: 1, backgroundColor: "#2e2e2e" }}
+        />
+        <StatusBar />
+      </SafeAreaView>
     </View>
   );
 }
@@ -11,8 +21,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#2e2e2e",
   },
 });
