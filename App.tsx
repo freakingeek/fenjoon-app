d6c56b1aa682b0eab7a56b056f4cda0c86a8d52b
Yasin Silavi <ys.silavi@gmail.com>
2025-03-13 00:02:22 +0330

    fix: add style

--- App.tsx
+++ App.tsx
@@ -54,7 +54,7 @@ export default function App() {
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#2e2e2e" }}>
-      <View>
+      <View style={{ flex: 1 }}>
         <WebView
           bounces={false}
           ref={webViewRef}
