6f805c38ba32455908e50c6358980fe1428017db
Yasin Silavi <ys.silavi@gmail.com>
2025-03-14 21:01:31 +0330

    feat: handle share story image

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
+import * as Sharing from 'expo-sharing'
 import { StatusBar } from "expo-status-bar";
-import { WebView } from "react-native-webview";
+import * as FileSystem from "expo-file-system";
+import { View, BackHandler } from "react-native";
 import React, { useRef, useState, useEffect } from "react";
-import { View, StyleSheet, BackHandler } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import WebView, { type WebViewMessageEvent } from "react-native-webview";
 
 export default function App() {
   const webViewRef = useRef(null);
@@ -28,9 +30,45 @@ export default function App() {
     };
   }, [canGoBack]);
 
+  const share = async (data: { url?: string }) => {
+    if (!data.url) return;
+
+    try {
+      const base64Data = data.url.split(",")[1];
+      const filePath = `${
+        FileSystem.cacheDirectory
+      }/temp_image_${Date.now()}.png`;
+
+      await FileSystem.writeAsStringAsync(filePath, base64Data, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+
+      if (!(await Sharing.isAvailableAsync())) {
+        return;
+      }
+
+      await Sharing.shareAsync(filePath, {
+        mimeType: "image/png",
+        dialogTitle: "اشتراک‌گذاری داستان",
+        UTI: "image.png",
+      });
+    } catch {}
+  };
+
+  const handleMessage = (event: WebViewMessageEvent) => {
+    const { type, ...payload } = JSON.parse(event.nativeEvent.data) as Record<
+      string,
+      string
+    >;
+
+    if (type === "share") {
+      share(payload);
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#2e2e2e" }}>
-      <View style={styles.container}>
+      <View style={{ flex: 1 }}>
         <WebView
           bounces={false}
           ref={webViewRef}
@@ -38,16 +76,10 @@ export default function App() {
           source={{ uri: "https://fenjoon.vercel.app" }}
           style={{ flex: 1, backgroundColor: "#2e2e2e" }}
           onNavigationStateChange={({ canGoBack }) => setCanGoBack(canGoBack)}
+          onMessage={handleMessage}
         />
         <StatusBar />
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#2e2e2e",
-  },
-});
