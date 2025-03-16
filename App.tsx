c99cd287e8aa5b7d793f20f1227b858c8107ef9d
Yasin Silavi <ys.silavi@gmail.com>
2025-03-16 05:33:15 +0330

    feat: add background color for main view

--- App.tsx
+++ App.tsx
@@ -68,7 +68,7 @@ export default function App() {
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#2e2e2e" }}>
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: "#2e2e2e" }}>
         <WebView
           bounces={false}
           ref={webViewRef}
