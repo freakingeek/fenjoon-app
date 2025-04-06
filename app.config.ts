d0def4098e35b1340ffee85abd06560fea5066d0
Yasin Silavi <ys.silavi@gmail.com>
2025-04-07 01:31:47 +0330

    chore: update version to 0.1.5

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ import { ExpoConfig } from "expo/config";
 const config: ExpoConfig = {
   name: "Fenjoon",
   slug: "fenjoon",
-  version: "0.1.4",
+  version: "0.1.5",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
